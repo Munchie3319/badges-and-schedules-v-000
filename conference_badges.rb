4269f0c24ac4ae25dabb865e60db6ec0cb10b5b0
rosa lee <rosalee0116@gmail.com>
2018-09-13 21:58:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  batch_badges = []
+  attendees.each do |attendees|
+    batch_badges.push(badge_maker(attendee))
+  end
+  return batch_badges
+end
